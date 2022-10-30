80c2a32278b432ba631b1df6683c93a4047b2652
hjc-love-code <jonathan.hjc@hotmail.com>
2022-10-30 17:23:29 +0800

    Build a directory structure

---
+++ PowerTools.cpp
@@ -0,0 +1,7 @@
+#include<bits/stdc++.h>
+#include"PowerTools_Data\Modules\import.h"
+using namespace std;
+int main() {
+    string ret = getLocation("shit");
+    cout << ret;
+}
---
+++ PowerTools_Data/Modules/move_pro.cpp
@@ -0,0 +1,7 @@
+#include<bits/stdc++.h>
+#include"import.h"
+using namespace std;
+string getLocation(string name) {
+    string fullname = name;
+    return fullname;
+}
