befb41e7da98099fd739781546379477fec23810
hjc-love-code <jonathan.hjc@hotmail.com>
2022-10-30 18:56:00 +0800

    finish program framework

---
+++ PowerTools_Data/Modules/help.cpp
@@ -0,0 +1,7 @@
+#include<bits/stdc++.h>
+#include"import.h"
+using namespace std;
+void helps() {
+    cout << R"deli([help] For command Line, you can use these features: 
+    moveApps -- To move program folder to other disk or location to safe space)deli";
+}
--- PowerTools_Data/Modules/import.h
+++ PowerTools_Data/Modules/import.h
@@ -1,3 +1,4 @@
 #include<bits/stdc++.h>
 using namespace std;
 string getLocation(string name);
+void helps();
